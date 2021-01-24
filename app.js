bdbc7bdad2766ea5f86b0bf2245d0882b3d40585
elalimardani <elnaz.alimardani@gmail.com>
2021-01-24 10:46:39 -0500

    output each new item to ejs file after a post request

---
+++ app.js
@@ -0,0 +1,33 @@
+//jshint esversion: 6
+const express = require('express');
+const bodyParser = require('body-parser');
+let items =[];
+
+const app = new express();
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', (req, res)=>{
+    let today = new Date();
+    let options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long',
+        year: 'numeric'
+    };
+    let day = today.toLocaleDateString('en-US', options);
+    res.render("list", {day: day, newListItems :items});
+});
+
+
+app.post('/', (req, res)=>{
+    items.push(req.body.newItem);
+    res.redirect('/');
+});
+
+
+
+app.listen(3000, ()=>{
+    console.log('listening on 3000');
+});
+
