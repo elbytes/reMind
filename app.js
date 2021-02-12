dc20a2394d4d636c4c70a63c601936156b5777d4
elbytes <elnaz.alimardani@gmail.com>
2021-02-12 17:20:28 -0500

    custom lists/add items to custom lists

--- app.js
+++ app.js
@@ -12,18 +12,28 @@ app.use(express.static('public'));
 
 mongoose.connect('mongodb://localhost:27017/todolistDB', 
     {useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false});
+//items schema
 const itemsSchema ={
     name: String
 };
+
 const Item = mongoose.model('item', itemsSchema);
+const day = date.getDate();
 const item1 = new Item({name: "Welcome to your todo list!"});
 const item2 = new Item({name: "Hit the + to add a new item"});
 const item3 = new Item({name: "<-- Hit the when you've finished an item"});
 const defaultItems = [item1, item2, item3];
 
+//list schema
+const listSchema = {
+    name: String,
+    items: [itemsSchema]
+}
+
+const List = mongoose.model('List', listSchema);
+
 //get list
 app.get('/', (req, res)=>{
-    const day = date.getDate();
     Item.find({}, (err, respose)=>{
         if(err){
             console.log(err);
@@ -38,7 +48,7 @@ app.get('/', (req, res)=>{
                 });
                 res.redirect('/');
             } else
-            res.render("list", {day: day, newListItems :respose});
+            res.render("list", {listTitle: day, newListItems :respose});
         }
     });
 });
@@ -46,9 +56,23 @@ app.get('/', (req, res)=>{
 //add items
 app.post('/', (req, res)=>{
     const itemsName = req.body.newItem;
+    const listName = req.body.list;
+
     const newItem = new Item({name: itemsName});
-    newItem.save();
-    res.redirect('/');
+    //check to see if the list is a custom or default list
+    if(listName === day){
+        newItem.save();
+        res.redirect('/');
+    } else{
+        //find the list and save it into the array of items on the list
+        List.findOne({name: listName}, (err, foundList)=>{
+            foundList.items.push(newItem);
+            foundList.save();
+            //redirect back to the list url
+            res.redirect('/' + listName);
+        })
+    }
+    
 });
 
 //delete from list
@@ -64,8 +88,27 @@ app.post('/delete', (req, res)=>{
     res.redirect('/');
 });
 
-app.get('/:list', (req, res)=>{
-    console.log(req.params.list);
+//custom lists
+app.get('/:customList', (req, res)=>{
+    const listName = req.params.customList;
+    List.findOne({name: listName}, (err, result)=>{
+         if(!err){
+             if(result){
+                 //show list
+                 console.log('found');
+                 res.render("list", {listTitle: result.name, newListItems :result.items})
+             }else{
+                 //create new list
+                const list = new List({
+                    name: listName,
+                    items: defaultItems
+                });
+                list.save();
+                res.redirect('/');
+                console.log('new list created');
+             }
+         }
+     })
 });
 
 app.listen(3000, ()=>{
